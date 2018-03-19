001ea17b3a99cad484431e3b8bb11c380dac4957
Hidayet Ok <hidonet@gmail.com>
2018-03-20 01:11:43 +0300

    Fix for multi stock_id inventory data
    Fix for multi stock_id inventory data

--- app/code/community/RicardoMartins/OutofstockLast/Model/CatalogSearch/Layer.php
+++ app/code/community/RicardoMartins/OutofstockLast/Model/CatalogSearch/Layer.php
@@ -11,19 +11,29 @@ class RicardoMartins_OutofstockLast_Model_CatalogSearch_Layer extends Mage_Catal
             ->addTaxPercents()
             ->addStoreFilter()
             ->addUrlRewrite();
-        $collection->getSelect()->joinLeft(
-                array('_inventory_table'=>'cataloginventory_stock_item'),
-                "_inventory_table.product_id = e.entity_id",
+
+		Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInSearchFilterToCollection($collection);
+        
+		$collection->getSelect()->joinLeft(
+                array('_inventory_table'=>'cataloginventory_stock_item'), 
+				"_inventory_table.product_id = e.entity_id",
                 array('is_in_stock', 'manage_stock')
             );
+
         $collection->addExpressionAttributeToSelect('on_top',
             '(CASE WHEN (((_inventory_table.use_config_manage_stock = 1)
             AND (_inventory_table.is_in_stock = 1)) OR  ((_inventory_table.use_config_manage_stock = 0)
             AND (1 - _inventory_table.manage_stock + _inventory_table.is_in_stock >= 1)))
-            THEN 1 ELSE 0 END)',
+            THEN 1 ELSE 0 END)
+			',
         array());
-        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
-        Mage::getSingleton('catalog/product_visibility')->addVisibleInSearchFilterToCollection($collection);
-        return $this;
-    }
+
+		$collection->setOrder('on_top', 'DESC');
+
+		$collection->getSelect()->group(array('entity_id'));
+
+		return $this;
+
+	}
 }
